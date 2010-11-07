c5cce28e4f6ed99741f98c6d0900679214b30d8a
mat <matthew.kelcey@gmail.com>
2010-11-07 20:19:59 +1100

    ouch, subtle bug re: int vs float division

--- tf_icf.rb
+++ tf_icf.rb
@@ -15,7 +15,7 @@ STDIN.each do |text|
 
   terms.uniq.sort.each do |token| 
     lhs = Math.log(1+freq[token])
-    rhs = Math.log((n+1)/(corpus_freq[token]+1))
+    rhs = Math.log((n+1.0)/(corpus_freq[token]+1))
     weight = lhs * rhs
     # printf "token %20s tf=%5i cf=%5i lhs=%0.3f rhs=%0.3f weight=%0.3f\n", t, freq[t], corpus_freq[t], lhs, rhs, weight
     puts [n,token,weight].join("\t")
